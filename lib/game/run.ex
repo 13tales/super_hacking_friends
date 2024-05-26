fa89f09c1fabcd0e11863c754b3d438f7047cd5e
Tom Armstrong <thomas.armstrong@me.com>
2024-05-26 19:24:35 +1000

    Refactored for multiple players; style tinkering; game logic tests

--- lib/game/run.ex
+++ lib/game/run.ex
@@ -1,6 +1,15 @@
 defmodule Game.Run do
+  defmodule Player do
+    defstruct name: "", commands: Map.new(), hits: 0, misses: 0, target: nil
+  end
+
   defmodule State do
-    defstruct player_count: 1, life: 100, ms_words: nil, finish_at: nil, target: nil
+    defstruct players: %{},
+              running: false,
+              life: 100,
+              finish_at: nil,
+              all_targets: Map.new(),
+              all_commands: Map.new()
   end
 
   @reward 5
@@ -8,70 +17,111 @@ defmodule Game.Run do
   @command_interval :timer.seconds(6)
   @max_level 10
   @run_length :timer.seconds(30)
+  @hand_limit 9
 
+  require Logger
+  import Access, only: [key!: 1]
+  alias Game.ProcServer.Proc
   alias Game.ProcServer
   use GenServer
 
   # Interface
-  def start(%State{} = init_state \\ %State{}) do
+  def new_game(%State{} = init_state \\ %State{}) do
     ProcServer.start()
-    GenServer.start(__MODULE__, init_state, name: __MODULE__)
+    GenServer.start(__MODULE__, init_state)
   end
 
-  def command(input) do
-    GenServer.call(__MODULE__, {:command, input})
+  def start_game(pid) do
+    GenServer.call(pid, :start_game)
   end
 
-  def get_state() do
-    GenServer.call(__MODULE__, :get_state)
+  def add_lobby_player(pid, name) do
+    GenServer.call(pid, {:add_player, name})
   end
 
-  def stop() do
-    GenServer.stop(__MODULE__)
+  def remove_lobby_player(pid, name) do
+    GenServer.call(pid, {:remove_player, name})
+  end
+
+  def command(pid, input) do
+    GenServer.call(pid, {:command, input})
+  end
+
+  def get_state(pid) do
+    GenServer.call(pid, :get_state)
+  end
+
+  def stop(pid) do
+    GenServer.stop(pid)
   end
 
   # Callbacks
   def init(%State{} = init_state \\ %State{}) do
+    {:ok, init_state}
+  end
+
+  def handle_call({:add_player, new_player}, _from, state) do
+    case add_player(state, new_player) do
+      {:ok, new_state} -> {:reply, {:ok, new_state}, new_state}
+      {:err, reason} -> {:reply, {:err, reason}, state}
+    end
+  end
+
+  def handle_call({:remove_player, name}, _from, state) do
+    new_state = remove_player(state, name)
+    {:reply, {:ok, new_state}, new_state}
+  end
+
+  def handle_call(:start_game, _from, state) do
     finish_time = DateTime.utc_now() |> DateTime.add(@run_length, :millisecond)
+
+    new_state =
+      %{state | running: true, finish_at: finish_time}
+      |> deal_all()
+      |> assign_all()
 
     :timer.send_interval(@run_length, :tick)
 
-    {:ok, %{init_state | finish_at: finish_time} |> set_new_target()}
+    {:reply, {:ok, new_state}, new_state}
   end
 
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
   end
 
-  def handle_call({:command, input}, _from, state) do
-    case run_proc(state, input) do
+  def handle_call({:command, input, player_name}, _from, state) do
+    case handle_input(state, player_name, input) do
       {_result, %State{life: 0} = new_state} ->
+        Logger.info("Game over! Players lose!")
         {:stop, :normal, :game_lost, new_state}
 
       {result, new_state} ->
-        {:reply, result, new_state |> set_new_target()}
+        {:reply, result, new_state}
     end
   end
 
-  def handle_info({:proc_timeout, target}, %State{target: current} = state) do
-    case target == current do
-      true ->
-        IO.puts("Timeout!")
+  def handle_info({:proc_timeout, target}, %State{} = state) do
+    Logger.info("Timeout for target: #{ProcServer.format(target)}")
+    target_key = ProcServer.format(target)
 
-        {:noreply,
-         damage(state, @damage)
-         |> set_new_target()}
+    case check_target_match(state, target_key) do
+      {:match, owner, _old_target} ->
+        state
+        |> damage(@damage)
+        |> assign_new_target(owner)
 
-      false ->
-        {:noreply, state}
+      :nomatch ->
+        state
     end
   end
 
   def handle_info(:tick, state) do
+    Logger.info("Tick")
+
     if DateTime.after?(DateTime.utc_now(), state.finish_at) do
       IO.puts("You win! Life remaining: #{state.life}")
 
-      {:stop, :normal, state }
+      {:stop, :normal, state}
     else
       {:noreply, state}
     end
@@ -79,23 +129,103 @@ defmodule Game.Run do
 
   # Game logic
 
+  # Remove a player from the game
+  def remove_player(state, name) do
+    Map.update!(state, :players, &Map.delete(&1, name))
+  end
+
+  # Add a player to the game
+  def add_player(state, new_name) do
+    case state do
+      %{running: false, players: players} when is_map_key(players, new_name) ->
+        {:err, :exists}
+
+      %{running: false} ->
+        new_state = put_in(state, [key!(:players), new_name], %Player{name: new_name})
+        {:ok, new_state}
+
+      %{running: true} ->
+        {:err, :in_progress}
+    end
+  end
+
+  # Deal random commands to all players, up to the hand limit
+  def deal_all(%State{} = state) do
+    new_players =
+      for {name, data} <- state.players, into: %{} do
+        {name,
+         %{
+           data
+           | commands:
+               Map.new(1..@hand_limit, fn _ ->
+                 cmd = ProcServer.pick(@max_level)
+                 {ProcServer.format(cmd), cmd}
+               end)
+         }}
+      end
+
+    command_pool =
+      Enum.reduce(new_players, %{}, fn {_name, player}, acc -> Map.merge(player.commands, acc) end)
+
+    %{state | players: new_players, all_commands: command_pool}
+  end
+
+  # Assign targets for all players
+  def assign_all(%State{players: players, all_commands: all_commands} = state) do
+    new_players =
+      players
+      |> Enum.map(fn {name, data} ->
+        {_, new_target} = Enum.random(all_commands)
+        {name, %{data | target: new_target}}
+      end)
+      |> Map.new()
+
+    new_target_pool =
+      Map.values(new_players)
+      |> Enum.map(fn %Player{target: target, name: player_name} ->
+        {ProcServer.format(target), {target, player_name}}
+      end)
+      |> Map.new()
+
+    # For each new target; send a timeout message after the designated interval
+    for {_key, {new_target, _name}} <- new_target_pool do
+      Process.send_after(self(), {:proc_timeout, new_target}, @command_interval)
+    end
+
+    %{state | players: new_players, all_targets: new_target_pool}
+  end
+
+  # Subtract health
   def damage(%State{} = state, hit) when is_integer(hit),
     do: Map.update(state, :life, 0, &max(0, &1 - hit))
 
+  # Increase health/score
   def score(%State{} = state, points) when is_integer(points),
     do: Map.update(state, :life, 0, &min(100, &1 + points))
 
-  def set_new_target(%State{} = state) do
-    new_target = ProcServer.pick(@max_level)
+  # Pick a new random target for the specified player name
+  def assign_new_target(%State{} = state, player_name)
+      when is_map_key(state.players, player_name) do
+    current_target = get_in(state, [key!(:players), player_name, key!(:target)])
+
+    {new_key, new_target} = Enum.random(state.all_commands)
+
+    Logger.info("New target for #{player_name}: #{new_key}")
+
+    new_state =
+      put_in(state, [key!(:players), player_name, key!(:target)], new_target)
+      |> Map.update!(:all_targets, fn ts ->
+        Map.delete(ts, ProcServer.format(current_target))
+        |> Map.put(new_key, {new_target, player_name})
+      end)
 
     Process.send_after(self(), {:proc_timeout, new_target}, @command_interval)
 
-    IO.puts("New target: #{ProcServer.format(new_target, true)}")
-
-    %{state | target: new_target}
+    new_state
   end
 
-  def check_command(proc, input)
+  # Check if a typed command matches a proc data structure
+  def input_matches_proc(proc, input)
       when is_binary(input) do
     target = ProcServer.format(proc)
 
@@ -104,21 +234,52 @@ defmodule Game.Run do
     String.equivalent?(target, clean_input)
   end
 
-  def run_proc(%State{target: target} = state, command) when is_binary(command) do
-    case check_command(target, command) do
-      true ->
-        IO.puts(
-          "Succeeded! Target: #{ProcServer.format(target)}, Input: #{command}, Reward: #{@reward}"
-        )
+  # Replace a command belonging to a named player
+  def replace_player_command(%State{} = state, %Proc{} = command, player_name)
+      when is_map_key(state.players, player_name) do
+    current_key = ProcServer.format(command)
+    new_command = ProcServer.pick(@max_level)
+    new_key = ProcServer.format(new_command)
 
-        {:success, score(state, @reward)}
+    state
+    |> update_in([Access.key!(:players), player_name, Access.key!(:commands)], fn cs ->
+      cs |> Map.delete(current_key) |> Map.put(new_key, new_command)
+    end)
+    |> update_in([:all_commands], fn cs ->
+      cs |> Map.delete(current_key) |> Map.put(new_key, new_command)
+    end)
+  end
 
-      false ->
-        IO.puts(
-          "Failed! Target: #{ProcServer.format(target)}, Input: #{command}, Damage: #{@damage}"
-        )
+  # Check if a typed command matches *any* current target, and return the name of the player it belongs to
+  def check_target_match(%State{all_targets: target_pool} = state, input) when is_binary(input) do
+    clean_input = input |> String.downcase() |> String.trim() |> String.replace("_", " ")
 
-        {:failure, damage(state, @damage)}
+    case Map.get(target_pool, clean_input) do
+      {target, player_name} ->
+        {:match, player_name, target}
+
+      nil ->
+        :nomatch
+    end
+  end
+
+  # Respond to an input event from a player
+  def handle_input(%State{players: players} = state, player_name, input)
+      when is_binary(input) and is_map_key(players, player_name) do
+    case check_target_match(state, input) do
+      {:match, owner, command} ->
+        new_state =
+          state
+          |> score(@reward)
+          |> replace_player_command(command, player_name)
+          |> assign_new_target(owner)
+
+        {:match, new_state}
+
+      :nomatch ->
+        new_state = damage(state, @damage)
+
+        {:nomatch, new_state}
     end
   end
 end
---
+++ test/game/run_test.exs
@@ -0,0 +1,196 @@
+defmodule Game.RunTest do
+  use ExUnit.Case
+  alias Game.ProcServer
+  alias Game.Run
+
+  describe "new_game/0" do
+    test "Starts the game server" do
+      {:ok, pid} = Run.new_game()
+
+      assert Process.alive?(pid)
+      Run.stop(pid)
+    end
+  end
+
+  describe "add_player/2" do
+    test "Adds a player when the name is unique" do
+      {_, state} =
+        %Run.State{}
+        |> Run.add_player("player1")
+        |> (fn {:ok, state} -> Run.add_player(state, "player2") end).()
+
+      assert Enum.count(state.players) == 2
+    end
+
+    test "Fails to add a player when the name already exists" do
+      {:ok, state} = Run.add_player(%Run.State{}, "player1")
+      {:err, :exists} = Run.add_player(state, "player1")
+    end
+  end
+
+  describe "remove_lobby_player/2" do
+    test "Removes a player" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, state} = Run.add_lobby_player(pid, "player2")
+
+      assert Enum.count(state.players) == 2
+
+      {:ok, state} = Run.remove_lobby_player(pid, "player1")
+      assert Enum.count(state.players) == 1
+
+      Run.stop(pid)
+    end
+  end
+
+  describe "start_game/1" do
+    test "Sets running to true when the game starts" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, _state} = Run.add_lobby_player(pid, "player2")
+
+      {:ok, state} = Run.start_game(pid)
+      assert state.running == true
+
+      Run.stop(pid)
+    end
+
+    test "Every player has a non-nil target after the game starts" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, _state} = Run.add_lobby_player(pid, "player2")
+      {:ok, _state} = Run.add_lobby_player(pid, "player3")
+
+      {:ok, state} = Run.start_game(pid)
+      assert state.running == true
+
+      Enum.each(state.players, fn {_, player} ->
+        assert player.target != nil
+      end)
+
+      Run.stop(pid)
+    end
+
+    test "All targets in state.all_targets match player targets" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, _state} = Run.add_lobby_player(pid, "player2")
+      {:ok, _state} = Run.add_lobby_player(pid, "player3")
+
+      {:ok, state} = Run.start_game(pid)
+      assert state.running == true
+
+      player_targets =
+        Enum.map(state.players, fn {_, player} -> player.target end) |> MapSet.new()
+
+      assert Enum.sort(
+               state.all_targets
+               |> Map.values()
+               |> Enum.map(&elem(&1, 0))
+               |> MapSet.new()
+             ) ==
+               Enum.sort(player_targets)
+
+      Run.stop(pid)
+    end
+
+    test "state.all_commands is equal to the superset of every player's :commands value" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, _state} = Run.add_lobby_player(pid, "player2")
+      {:ok, _state} = Run.add_lobby_player(pid, "player3")
+
+      {:ok, state} = Run.start_game(pid)
+      assert state.running == true
+
+      player_commands =
+        Enum.flat_map(state.players, fn {_, player} -> player.commands end) |> MapSet.new()
+
+      assert MapSet.equal?(MapSet.new(state.all_commands), player_commands)
+
+      Run.stop(pid)
+    end
+
+    test "Every player target is a command belonging to another player" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, _state} = Run.add_lobby_player(pid, "player2")
+      {:ok, _state} = Run.add_lobby_player(pid, "player3")
+
+      {:ok, state} = Run.start_game(pid)
+      assert state.running == true
+
+      player_commands =
+        Enum.flat_map(state.players, fn {_, player} ->
+          Enum.map(player.commands, &elem(&1, 1))
+        end)
+        |> MapSet.new()
+
+      Enum.each(state.players, fn {_, player} ->
+        assert MapSet.member?(player_commands, player.target)
+      end)
+
+      Run.stop(pid)
+    end
+  end
+
+  describe "assign_new_target/2" do
+    test "Assigns a new target that is a command belonging to another player" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, _state} = Run.add_lobby_player(pid, "player2")
+      {:ok, _state} = Run.add_lobby_player(pid, "player3")
+
+      {:ok, state} = Run.start_game(pid)
+      assert state.running == true
+
+      state = Run.assign_new_target(state, "player1")
+
+      player_commands =
+        Enum.flat_map(state.players, fn {_, player} ->
+          Enum.map(player.commands, &elem(&1, 1))
+        end)
+        |> MapSet.new()
+
+      player1 = state.players["player1"]
+      assert MapSet.member?(player_commands, player1.target)
+
+      Run.stop(pid)
+    end
+
+    test "New target is stored in state.all_targets with correct tuple format" do
+      {:ok, pid} = Run.new_game()
+      assert Process.alive?(pid)
+
+      {:ok, _state} = Run.add_lobby_player(pid, "player1")
+      {:ok, _state} = Run.add_lobby_player(pid, "player2")
+      {:ok, _state} = Run.add_lobby_player(pid, "player3")
+
+      {:ok, state} = Run.start_game(pid)
+      assert state.running == true
+
+      state = Run.assign_new_target(state, "player1")
+
+      player1 = state.players["player1"]
+      target_tuple = {player1.target, "player1"}
+
+      assert Map.get(state.all_targets, ProcServer.format(player1.target)) == target_tuple
+
+      Run.stop(pid)
+    end
+  end
+end
